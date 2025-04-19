687e9df1c6febbf071517c730a1c7f4ffbb3d6cb
geetoor-maven <agus40994@gmail.com>
2025-04-20 01:06:22 +0700

    FEAT - SPRING-BOOT CHECKSTYLE

---
+++ src/main/java/com/checkstyle/SpringbootCheckstyleApplication.java
@@ -0,0 +1,13 @@
+package com.checkstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCheckstyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCheckstyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkstyle/SpringbootCheckstyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCheckstyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
