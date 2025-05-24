10a22f7d393cf53845c287d3d387729b7c8297fb
geetoor-maven <agus40994@gmail.com>
2025-05-25 01:32:41 +0700

    FEAT - SPRING-BOOT CHECKSTYLE

--- src/main/java/com/checkstyle/SpringbootCheckstyleApplication.java
+++ src/main/java/com/checkstyle/SpringbootCheckstyleApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootCheckstyleApplication {
 
+	/**
+	 * Entry point untuk menjalankan aplikasi Spring Boot.
+	 *
+	 * @param args argumen baris perintah
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCheckstyleApplication.class, args);
 	}
--- src/main/java/com/checkstyle/controller/HelloApp.java
+++ src/main/java/com/checkstyle/controller/HelloApp.java
@@ -1,20 +1,22 @@
 package com.checkstyle.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * hello app is class for test app
+ * HelloApp adalah kelas controller yang digunakan untuk menguji aplikasi.
  */
 @RestController
-public class HelloApp{
+public class HelloApp {
 
-    @GetMapping(
-            path = "/hello-app"
-    )
+    /**
+     * Endpoint untuk mengembalikan pesan "check style app".
+     *
+     * @return String pesan sambutan
+     */
+    @GetMapping(path = "/hello-app")
     public String helloApp() {
         return "check style app";
     }
+
 }
